099f86448215c40566fda04021920a8f8e6b8370
Alanyu <squid78313@gmail.com>
2024-12-20 12:28:56 +0800

    修改rate-limit錯誤訊息回傳

--- middlewares/rate-limit.js
+++ middlewares/rate-limit.js
@@ -3,13 +3,19 @@ const rateLimit = require('express-rate-limit');
 const emailLimiter = rateLimit({
   windowMs: 60 * 1000, // 每 1 分鐘
   max: 3, // 只允許 3 次請求
-  message: "Too many requests, please try again later."
+  message: {
+    success: false,
+    status: 429,
+    message: "Too many requests, please try again later.",
+  }
 });
 
 const smsLimiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 每 2 分鐘
   max: 1, // 只允許 1 次請求
   message: {
+    success: false,
+    status: 429,
     message: "Too many requests, please try again later.",
   }
 });
