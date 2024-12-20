f97f112cd7fc116fa1cb817950f4341db6843cd4
Alanyu <squid78313@gmail.com>
2024-12-20 10:19:37 +0800

    加入express-rate-limit速率限制請求

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../../controllers/user-controller');
+const { emailLimiter, smsLimiter } = require('../../../middlewares/rate-limit');
 
 // 取得使用者資料
 router.get('/profile/:id', userController.getUser)
 // 發送手機驗證碼
-router.post('/phone-code', userController.sendPhoneVerification)
+router.post('/phone-code',smsLimiter, userController.sendPhoneVerification)
 // 發送驗證email
-router.post('/email-code', userController.sendEmailVerification)
+router.post('/email-code',emailLimiter, userController.sendEmailVerification)
 // 驗證email
 router.post('/verification/email', userController.verifyEmail)
 
