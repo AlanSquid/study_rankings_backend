45b166cfc4336b64f6faf5908b9003fec234daa7
Alanyu <squid78313@gmail.com>
2024-12-30 10:56:17 +0800

    create UniversityGroup model

---
+++ models/universitygroup.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UniversityGroup extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  UniversityGroup.init({
+    name: DataTypes.STRING(50)
+  }, {
+    sequelize,
+    modelName: 'UniversityGroup',
+    tableName: 'university_groups'
+  });
+  return UniversityGroup;
+};
---
+++ seeders/constants/20241230024925-create-university-groups.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const universityGroups = [
+      { name: 'Group of 8' },
+      { name: 'ATN' },
+      { name: 'IRU' },
+    ]
+    await queryInterface.bulkInsert('university_groups', universityGroups, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('university_groups', null, {});
+  }
+};
